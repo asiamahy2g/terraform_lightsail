af476e8a44811f61fdb18dfe88936b4fac1fc94d
Emmanuel <asiamahy2g@gmail.com>
2023-12-15 20:51:57 -0600

    created install.sh file

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@
 output "lightsail_instance_ip" {
   value = aws_lightsail_instance.light_sail_1.public_ip_address
 }
+
+output "ssh-command" {
+  value = "ssh -i keypair.pem ec2-user@${aws_lightsail_instance.light_sail_1.public_ip_address}"
+}
